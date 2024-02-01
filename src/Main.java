9864ea7465935445c92887a413331aedc8ba2b06
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-02-01 11:09:37 -0500

    buildFix

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
 public class Main {
-    public static void main(String[] args) {
+    public Main() {
+    }
 
+    public static void main(String[] args) {
         BeanMaster threadSafeBeanMasterShared = new BeanMaster();
         threadSafeBeanMasterShared.init();
         threadSafeBeanMasterShared.makeCoffee(20);
