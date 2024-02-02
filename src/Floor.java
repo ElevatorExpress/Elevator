82a9aa2c33e98fa7bf95c34810fb0c4c3e94c2b5
Mayukh <mayukh.gautam@gmail.com>
2024-02-02 13:21:45 -0500

    Refactored Floor to better fit P1 specs.

--- src/Floor.java
+++ src/Floor.java
@@ -1,67 +1,48 @@
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.ArrayList;
 
-public class Floor implements Runnable{
-    private final ResourceType specialtyIngredient;
-
-    private final HashMap<ResourceType, ResourceType[]> resourceMap;
-
-    private final ThreadSafeResourceBuffer threadSafeResourceBuffer;
-
-    private int cofeeGoal = 0;
-
-    private final UtilityInterface<Integer> coffeeCounter;
+public class Floor implements Runnable, UtilityInterface<Floor.FloorInfo>{
+    private final UtilityInterface<FloorInfo> scheduler;
+    private final FloorInfo currentFloorInfo;
+    /** @noinspection MismatchedQueryAndUpdateOfCollection*/
+    private final ArrayList<FloorInfo> floorInfoCatcher;
 
 
-    public Floor(ResourceType specialtyIngredient, ThreadSafeResourceBuffer threadSafeResourceBuffer, UtilityInterface<Integer> coffeeCounter) {
-        this.coffeeCounter = coffeeCounter;
-        this.specialtyIngredient = specialtyIngredient;
-
-        resourceMap = new HashMap<>();
-
-        Arrays.stream(ResourceType.values())
-                .filter(type -> type != specialtyIngredient)
-                .forEach(type -> resourceMap.put(type, new ResourceType[]{null}));
-        this.threadSafeResourceBuffer = threadSafeResourceBuffer;
+    public Floor(UtilityInterface<FloorInfo> ownerScheduler) {
+        scheduler = ownerScheduler;
+        floorInfoCatcher = new ArrayList<>();
+        /*
+            this.floorInfo = FloorInfo.create( fName | Path );
+            This method is supposed to create floor info. Read from file.
+         */
+        // Temp
+        currentFloorInfo = new FloorInfo();
     }
-    public void setCoffeeGoal(int coffeeGoal){
-        this.cofeeGoal = coffeeGoal;
-    }
-
-
-    public void makeCoffee() throws InterruptedException {
-        int cCounter = coffeeCounter.get();
-//        boolean done = false;
-        while (true) {
-
-            ResourceType[] resources = threadSafeResourceBuffer.get(specialtyIngredient);
-            cCounter = coffeeCounter.put(1);
-            if (cCounter > cofeeGoal) {
-                break;
-            }
-            StringBuilder sb = new StringBuilder();
-            for (ResourceType r : resources) {
-                sb.append(r).append(", ");
-            }
-            sb.delete(sb.length() - 2, sb.length());
-
-
-            System.out.println("Floor made coffee number: " + cCounter + " with: " + sb + " from BeanMaster and " + specialtyIngredient + " from " + specialtyIngredient + " Floor");
-
-        }
-    }
-
-
 
     public void run(){
-        try {
-            makeCoffee();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        // Does this only have to work once ?
+        sendFloorData();
     }
+
+    //
+    private void sendFloorData(){
+        this.scheduler.put(this.currentFloorInfo);
+    }
+
+    /**
+     * Unused
+     *
+     * @return null
+     */
+    @Override
+    public FloorInfo get() {return null;}
+
+    @Override
+    public synchronized FloorInfo put(FloorInfo fInfo) {
+        floorInfoCatcher.add(fInfo);
+        return fInfo;
+    }
+
+    public record FloorInfo(){}
 
 
 }
