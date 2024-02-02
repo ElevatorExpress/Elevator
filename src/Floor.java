0d9f84ec016be2be52066c4d3e786706c6cdc366
Mayukh <mayukh.gautam@gmail.com>
2024-02-02 13:21:45 -0500

    Refactored Floor to better fit P1 specs.

--- src/Floor.java
+++ src/Floor.java
@@ -1,9 +1,5 @@
 import java.util.ArrayList;
 
-/**
- * Represents the Floor
- * @author Mayukh Gautam 101181018
- */
 public class Floor implements Runnable, UtilityInterface<Floor.FloorInfo>{
     private final UtilityInterface<FloorInfo> scheduler;
     private final FloorInfo currentFloorInfo;
@@ -15,10 +11,11 @@ public class Floor implements Runnable, UtilityInterface<Floor.FloorInfo>{
         scheduler = ownerScheduler;
         floorInfoCatcher = new ArrayList<>();
         /*
-            currentFloorInfo = new FloorInfo(Filelocation);
+            this.floorInfo = FloorInfo.create( fName | Path );
             This method is supposed to create floor info. Read from file.
          */
         // Temp
+        currentFloorInfo = new FloorInfo();
     }
 
     public void run(){
@@ -26,9 +23,7 @@ public class Floor implements Runnable, UtilityInterface<Floor.FloorInfo>{
         sendFloorData();
     }
 
-    /**
-     * Sends this floor's information to the scheduler
-     */
+    //
     private void sendFloorData(){
         this.scheduler.put(this.currentFloorInfo);
     }
@@ -39,16 +34,15 @@ public class Floor implements Runnable, UtilityInterface<Floor.FloorInfo>{
      * @return null
      */
     @Override
-    public FloorInfo get() {return currentFloorInfo;}
+    public FloorInfo get() {return null;}
 
-    /**
-     * Adds Floor information to this floor's floor information cache
-     */
     @Override
     public synchronized FloorInfo put(FloorInfo fInfo) {
         floorInfoCatcher.add(fInfo);
         return fInfo;
     }
+
+    public record FloorInfo(){}
 
 
 }
