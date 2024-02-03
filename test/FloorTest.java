6a0b1c0e8c582825e1a582e4ca7dacaee1efa8d6
Josh <jabraddon@gmail.com>
2024-02-03 11:46:02 -0500

    Added tests for file reading

--- test/FloorTest.java
+++ test/FloorTest.java
@@ -1,34 +1,38 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.util.Iterator;
 
+/**
+ * A class to test if FloorSystem can read FloorInfoReader data
+ * @author Joshua Braddon
+ */
 class FloorTest {
 
-    static Floor f;
+    static FloorInfoReader reader;
+    static final String[] requestExpected = {"2:432up3", "3:124down2", "5:151up5", "11:152up4", "6:163down1", "7:451up5"};
 
     @BeforeAll
-    static void createFloor(){
-        f = new Floor(new UtilityInterface<>() {
-            @Override
-            public Floor.FloorInfo get() {
-                return null;
-            }
-
-            @Override
-            public Floor.FloorInfo put(Floor.FloorInfo floorInfo) {
-                return null;
-            }
-        });
+    static void createFloorInfoReader(){
+        reader = new FloorInfoReader(new File("./FloorTest"));
     }
 
     @Test
-    void get() {
-        Assertions.assertNull(f.get());
-    }
+    static void readText(FloorInfoReader reader) {
+        Iterator<FloorInfoReader.Data> requests = reader.getRequestQueue();
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+        while(requests.hasNext()) {
+            //Adds the values from the Data ArrayList to a StringBuilder
+            sb.append(requests.next().time());
+            sb.append(requests.next().serviceFloor());
+            sb.append(requests.next().direction());
+            sb.append(requests.next().requestFloor());
 
-    @Test
-    void put() {
-        Object fInfo = f.put(new Floor.FloorInfo());
-        Assertions.assertNotNull(fInfo);
+            //Compares to the expected values
+            Assertions.assertEquals(sb.toString(), requestExpected[i], "File did not read correctly, expected: " + requestExpected[i] + " actual: ");
+            i++;
+        }
     }
 }
