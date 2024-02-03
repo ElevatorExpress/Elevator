168e2697ba45d43c4089fe0eee3697a653dcd6c4
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-02-03 18:45:51 -0500

    merge

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 // (powered by FernFlower decompiler)
 //
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public Main() {
     }
@@ -12,5 +14,23 @@ public class Main {
 //        MessageBuffer threadSafeResourceBuffer = new MessageBuffer(2);
 ////        threadSafeNotSchedulerShared.makeCoffee();
 //        threadSafeNotSchedulerShared.run();
+        MessageBuffer messageBuffer = new MessageBuffer(300);
+        MessageBuffer floorOutBuffer = new MessageBuffer(100);
+        MessageBuffer elevatorOutBuffer = new MessageBuffer(100);
+        FloorSystem floorSystem = new FloorSystem(floorOutBuffer, messageBuffer);
+        ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem(elevatorOutBuffer, messageBuffer);
+        Scheduler scheduler = new Scheduler(messageBuffer, floorOutBuffer, elevatorOutBuffer);
+
+        Thread floorSystemThread = new Thread(floorSystem);
+        Thread elevatorSubsystemThread = new Thread(elevatorSubsystem);
+        Thread schedulerThread = new Thread(scheduler);
+
+
+        floorSystemThread.start();
+        elevatorSubsystemThread.start();
+        schedulerThread.start();
+        System.out.println("Threads Started");
+        TimeUnit.SECONDS.sleep(5);
+        TimeUnit.SECONDS.sleep(5);
     }
 }
