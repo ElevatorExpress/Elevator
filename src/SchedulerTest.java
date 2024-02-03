c67825153f166b13bb829295557d8c9fc5ce1f12
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-02-03 14:56:01 -0500

    Scheduler refac

---
+++ src/SchedulerTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SchedulerTest {
+
+    @org.junit.jupiter.api.Test
+    void serveElevatorReqs() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void readBuffer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void serveFloorRequests() {
+    }
+
+
+
+}
