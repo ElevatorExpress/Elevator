ca4876305c837d92be83a59063ff8caf61608362
Mayukh <mayukh.gautam@gmail.com>
2024-02-03 13:49:21 -0500

    Refactored Message Factories to not contain the Messages they generate

--- src/Messages/ElevatorMessageFactory.java
+++ src/Messages/ElevatorMessageFactory.java
@@ -1,7 +1,6 @@
 package Messages;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 
 // if sending a message to the server, the data MUST include FloorMessage fulfilled. AT MINIMUM!
@@ -10,84 +9,6 @@ public class ElevatorMessageFactory<T> {
 
     public ElevatorMessage<T> createElevatorMessage(String elevatorID, Map<String, T> data, ElevatorSignal signal){
         return new ElevatorMessage<>(MessageTypes.ELEVATOR, elevatorID, data, signal, UUID.randomUUID().toString());
-    }
-
-    private record ElevatorMessage<T>(MessageTypes messageType, String elevatorID, Map<String, T> data, ElevatorSignal signal, String id) implements MessageInterface<T, ElevatorSignal>{
-
-        @Override
-        public MessageTypes getType() {
-            return messageType;
-        }
-
-        @Override
-        public ElevatorSignal getSignal() {
-            return signal;
-        }
-
-        @Override
-        public Map<String, T> getData() {
-            return data;
-        }
-
-        @Override
-        public String getSenderID() {
-            return elevatorID;
-        }
-
-        @Override
-        public String getMessageId() {
-            return id;
-        }
-
-        /**
-         * Indicates whether some other object is "equal to" this one.  In addition
-         * to the general contract of {@link Object#equals(Object) Object.equals},
-         * record classes must further obey the invariant that when
-         * a record instance is "copied" by passing the result of the record component
-         * accessor methods to the canonical constructor, as follows:
-         * <pre>
-         *     R copy = new R(r.c1(), r.c2(), ..., r.cn());
-         * </pre>
-         * then it must be the case that {@code r.equals(copy)}.
-         *
-         * @param obj the reference object with which to compare.
-         * @return {@code true} if this record is equal to the
-         * argument; {@code false} otherwise.
-         * @implSpec The implicitly provided implementation returns {@code true} if
-         * and only if the argument is an instance of the same record class
-         * as this record, and each component of this record is equal to
-         * the corresponding component of the argument; otherwise, {@code
-         * false} is returned. Equality of a component {@code c} is
-         * determined as follows:
-         * <ul>
-         *
-         * <li> If the component is of a reference type, the component is
-         * considered equal if and only if {@link
-         * Objects#equals(Object, Object)
-         * Objects.equals(this.c, r.c)} would return {@code true}.
-         *
-         * <li> If the component is of a primitive type, using the
-         * corresponding primitive wrapper class {@code PW} (the
-         * corresponding wrapper class for {@code int} is {@code
-         * java.lang.Integer}, and so on), the component is considered
-         * equal if and only if {@code
-         * PW.compare(this.c, r.c)} would return {@code 0}.
-         *
-         * </ul>
-         * <p>
-         * Apart from the semantics described above, the precise algorithm
-         * used in the implicitly provided implementation is unspecified
-         * and is subject to change. The implementation may or may not use
-         * calls to the particular methods listed, and may or may not
-         * perform comparisons in the order of component declaration.
-         * @see Objects#equals(Object, Object)
-         */
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof ElevatorMessage<?> elevatorMessage){
-                return Objects.equals(elevatorMessage.id, id);
-            } return false;
-        }
     }
 
 }
--- src/Messages/FloorMessageFactory.java
+++ src/Messages/FloorMessageFactory.java
@@ -1,7 +1,6 @@
 package Messages;
 
 import java.util.Map;
-import java.util.Objects;
 import java.util.UUID;
 
 public class FloorMessageFactory<T> {
@@ -10,81 +9,4 @@ public class FloorMessageFactory<T> {
         return new FloorMessage<>(MessageTypes.FLOOR, floorID, data, signal, UUID.randomUUID().toString());
     }
 
-    private record FloorMessage<T>(MessageTypes messageType, String floorID, Map<String, T> data, FloorSignal signal, String id) implements MessageInterface<T, FloorSignal>{
-
-        @Override
-        public MessageTypes getType() {
-            return messageType;
-        }
-
-        @Override
-        public FloorSignal getSignal() {
-            return signal;
-        }
-
-        @Override
-        public Map<String, T> getData() {
-            return data;
-        }
-
-        @Override
-        public String getSenderID() {
-            return floorID;
-        }
-
-        @Override
-        public String getMessageId() {
-            return id;
-        }
-
-        /**
-         * Indicates whether some other object is "equal to" this one.  In addition
-         * to the general contract of {@link Object#equals(Object) Object.equals},
-         * record classes must further obey the invariant that when
-         * a record instance is "copied" by passing the result of the record component
-         * accessor methods to the canonical constructor, as follows:
-         * <pre>
-         *     R copy = new R(r.c1(), r.c2(), ..., r.cn());
-         * </pre>
-         * then it must be the case that {@code r.equals(copy)}.
-         *
-         * @param obj the reference object with which to compare.
-         * @return {@code true} if this record is equal to the
-         * argument; {@code false} otherwise.
-         * @implSpec The implicitly provided implementation returns {@code true} if
-         * and only if the argument is an instance of the same record class
-         * as this record, and each component of this record is equal to
-         * the corresponding component of the argument; otherwise, {@code
-         * false} is returned. Equality of a component {@code c} is
-         * determined as follows:
-         * <ul>
-         *
-         * <li> If the component is of a reference type, the component is
-         * considered equal if and only if {@link
-         * Objects#equals(Object, Object)
-         * Objects.equals(this.c, r.c)} would return {@code true}.
-         *
-         * <li> If the component is of a primitive type, using the
-         * corresponding primitive wrapper class {@code PW} (the
-         * corresponding wrapper class for {@code int} is {@code
-         * java.lang.Integer}, and so on), the component is considered
-         * equal if and only if {@code
-         * PW.compare(this.c, r.c)} would return {@code 0}.
-         *
-         * </ul>
-         * <p>
-         * Apart from the semantics described above, the precise algorithm
-         * used in the implicitly provided implementation is unspecified
-         * and is subject to change. The implementation may or may not use
-         * calls to the particular methods listed, and may or may not
-         * perform comparisons in the order of component declaration.
-         * @see Objects#equals(Object, Object)
-         */
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof FloorMessage<?> floorMessage){
-                return Objects.equals(floorMessage.id, id);
-            } return false;
-        }
-    }
 }
