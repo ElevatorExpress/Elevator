52494ce2be2e9310c784a3f56b9fcf4285495851
Yasir Sheikh <ysheikh1234@gmail.com>
2024-02-03 16:17:12 -0500

    docs + test class

---
+++ test/ElevatorSubsystemTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElevatorSubsystemTest {
+
+    @Test
+    void receiveMessage() {
+    }
+
+    @Test
+    void sendMessage() {
+    }
+}
