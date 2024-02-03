8f51d34e873e32e9caaef27a52e39a7a392223c2
Mayukh <mayukh.gautam@gmail.com>
2024-02-03 10:13:15 -0500

    Updated FloorInfo reader to use multi line read

--- src/FloorSystem.java
+++ src/FloorSystem.java
@@ -5,6 +5,7 @@ import Messages.MessageInterface;
 import java.io.File;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 
 /**
  * Represents the Floor
@@ -14,33 +15,37 @@ public class FloorSystem implements Runnable, SubSystem<MessageInterface<String,
     private final FloorInfoReader currentFloorInfoReader;
     private ElevatorMessage<String> floorInfoMessage;
     /** @noinspection MismatchedQueryAndUpdateOfCollection*/
-    private final SubSystem<ElevatorMessage<?>> scheduler;
     private final MessageBuffer inboundMessageBuffer;
     private final MessageBuffer outboundMessageBuffer;
 
-    public FloorSystem(SubSystem<ElevatorMessage<?>> mainScheduler) {
-        scheduler = mainScheduler;
-        inboundMessageBuffer = new MessageBuffer(100);
-        outboundMessageBuffer = new MessageBuffer(100);
+    public FloorSystem(MessageBuffer outbound, MessageBuffer inbound) {
+
+        /*
+        new FloorMessage:UUID-1, FID -> Sch -> FloorMessage:UUID-1, FID -> Elevator -> new Elevator:UUID-2 -> Sch -> new FloorMessage:UUID-4, FID
+         */
+
+        inboundMessageBuffer = inbound;
+        outboundMessageBuffer = outbound;
         floorInfoMessage = null; // In case of error during real init
-        currentFloorInfoReader = new FloorInfoReader(new File("./elevator_requests.txt"));
+        currentFloorInfoReader = new FloorInfoReader(new File("./Floor data.txt"));
         createMessages();
     }
 
     private void createMessages(){
-        // Loop for each entry in file.
-        HashMap<String, String> floorDataMap = new HashMap<>();
-        floorDataMap.putIfAbsent("Time", currentFloorInfoReader.getTime());
-        floorDataMap.putIfAbsent("RequestFloor", currentFloorInfoReader.getRequestFloor());
-        floorDataMap.putIfAbsent("RequestDirection", currentFloorInfoReader.getDirection());
-        floorDataMap.putIfAbsent("Floor", currentFloorInfoReader.getServiceFloor());
-        floorInfoMessage = new ElevatorMessage<>(null, null, floorDataMap, ElevatorSignal.DONE/* TEMP. IT SHOULD BE: FloorSignal.WORK_REQ*/);
+        Iterator<FloorInfoReader.Data> iterator = currentFloorInfoReader.getRequestQueue();
+        while (iterator.hasNext()) {
+            FloorInfoReader.Data floorData = iterator.next();
+            HashMap<String, String> floorDataMap = new HashMap<>();
+            floorDataMap.putIfAbsent("Time", floorData.time());
+            floorDataMap.putIfAbsent("ServiceFloor", floorData.serviceFloor());
+            floorDataMap.putIfAbsent("RequestDirection", floorData.direction());
+            floorDataMap.putIfAbsent("Floor", floorData.requestFloor());
+            floorInfoMessage = new ElevatorMessage<>(null, null, floorDataMap, ElevatorSignal.DONE/* TEMP. IT SHOULD BE: FloorSignal.WORK_REQ*/);
+        }
     }
 
     private void prepareAndSendMessage(){
-        outboundMessageBuffer.put(new MessageInterface[]{floorInfoMessage});
-
-        sendMessage(outboundMessageBuffer.get());
+        //sendMessage();
     }
 
     public void run(){
@@ -51,13 +56,15 @@ public class FloorSystem implements Runnable, SubSystem<MessageInterface<String,
     @Override
     public void receiveMessage(MessageInterface<String, ElevatorSignal>[] message) {
         // Do nothing but store for It-1. This is the end of the message chain.
-        inboundMessageBuffer.put(message);
+        while (true) {
+            outboundMessageBuffer.get();
+        }
     }
 
     @Override
     public String[] sendMessage(MessageInterface<String, ElevatorSignal>[] message) {
         // Send to scheduler
-        scheduler.receiveMessage((ElevatorMessage<?>[]) message);
+        inboundMessageBuffer.put(message);
         return Arrays.stream(message).map(msg -> msg.getData().toString()).toArray(String[]::new);
     }
 
