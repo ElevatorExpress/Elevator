d345c786332a05c494c058ee2f4936acaaf2d8b6
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-02-06 22:09:58 -0500

    Sched Clean up

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,10 @@ public class Main {
     }
 
     public static void main(String[] args) throws InterruptedException {
-//        NotScheduler threadSafeNotSchedulerShared = new NotScheduler(20);
-//        MessageBuffer threadSafeResourceBuffer = new MessageBuffer(2);
-////        threadSafeNotSchedulerShared.makeCoffee();
-//        threadSafeNotSchedulerShared.run();
-        MessageBuffer messageBuffer = new MessageBuffer(300, " TO SCHED BUFFER");
-        MessageBuffer floorOutBuffer = new MessageBuffer(100, " TO FLOOR BUFFER ");
-        MessageBuffer elevatorOutBuffer = new MessageBuffer(100, " TO ELEVATOR BUFFER ");
+
+        MessageBuffer messageBuffer = new MessageBuffer(30, " TO SCHED BUFFER");
+        MessageBuffer floorOutBuffer = new MessageBuffer(30, " TO FLOOR BUFFER ");
+        MessageBuffer elevatorOutBuffer = new MessageBuffer(30, " TO ELEVATOR BUFFER ");
         FloorSystem floorSystem = new FloorSystem(floorOutBuffer, messageBuffer);
         ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem(messageBuffer,elevatorOutBuffer );
         Scheduler scheduler = new Scheduler(messageBuffer, floorOutBuffer, elevatorOutBuffer);
@@ -26,13 +23,11 @@ public class Main {
         Thread schedulerThread = new Thread(scheduler, "Scheduler");
 
 
-//        TimeUnit.SECONDS.sleep(1);
         elevatorSubsystemThread.start();
         floorSystemThread.start();
         TimeUnit.SECONDS.sleep(1);
         schedulerThread.start();
         System.out.println("Threads Started");
-//        TimeUnit.SECONDS.sleep(5);
 
 
     }
