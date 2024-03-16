358369395eea7792137b866802727b9381a0fb0b
Yasir Sheikh <ysheikh1234@gmail.com>
2024-03-16 16:19:14 -0400

    Elevator Subsystem Tests

--- test/ElevatorSubsystemTest.java
+++ test/ElevatorSubsystemTest.java
@@ -1,46 +1,90 @@
-import util.Messages.FloorMessageFactory;
-import util.Messages.MessageInterface;
+import floor.FloorInfoReader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import util.Messages.MessageTypes;
+import util.Messages.SerializableMessage;
 import util.Messages.Signal;
 import elevator.ElevatorSubsystem;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import util.MessageBuffer;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.net.*;
+import java.util.UUID;
+
+import static util.MessageHelper.ReceiveMessage;
+import static util.MessageHelper.SendMessage;
 
 class ElevatorSubsystemTest {
 
-    MessageBuffer sharedBuffer = new MessageBuffer(1, "");
-    ElevatorSubsystem elevator;
-    MessageInterface<String>[] request = new MessageInterface[1];
+    static MessageBuffer queue;
+    static ElevatorSubsystem elevator;
+    static DatagramSocket socket;
 
-    @BeforeEach
-    void createElevator(){
-        elevator = new ElevatorSubsystem(sharedBuffer, sharedBuffer);
-
-        HashMap<String, String> workData = new HashMap<>();
-        workData.put("Time", "14:05:15.0");
-        workData.put("ServiceFloor", "2");
-        workData.put("RequestDirection", "up");
-        workData.put("Floor", "4");
-        request[0] = new FloorMessageFactory<String>().createFloorMessage("FloorID-1", workData, Signal.WORK_REQ);
+    @BeforeAll
+    static void createElevatorSystem() {
+        InetAddress addr;
+        try {
+            addr = InetAddress.getLocalHost();
+            socket = new DatagramSocket(8081);
+            queue = new MessageBuffer(
+                    1,
+                    "ElevatorTest",
+                    socket,
+                    new InetSocketAddress(addr, 8080),
+                    8080
+            );
+        } catch (SocketException | UnknownHostException  ignored) {}
+        queue.listenAndFillBuffer();
+        elevator = new ElevatorSubsystem(queue, 1);
     }
 
     @Test
-    void sendMessage() {
-        elevator.sendMessage(request);
-        Assertions.assertEquals(1, sharedBuffer.getBufferLength());
+    void sendMessage(){
+        InetAddress addr = null;
+        try {
+            addr = InetAddress.getLocalHost();
+        }catch (UnknownHostException ignored){}
+        FloorInfoReader.Data data = new FloorInfoReader.Data("4:12", "2", "up", "9");
+
+        assert addr != null;
+        SerializableMessage sm =  new SerializableMessage(addr.getHostAddress(), 8081, Signal.DONE, MessageTypes.ELEVATOR, 1, null, UUID.randomUUID().toString(), data);
+
+        Thread t = new Thread(() -> {
+            try {
+                byte[] messageArray = new byte[1024];
+                DatagramSocket reviveSocket = new DatagramSocket(8080);
+                DatagramPacket receivePacket = new DatagramPacket(messageArray, messageArray.length);
+                SerializableMessage msg = ReceiveMessage(reviveSocket, messageArray, receivePacket);
+                Assertions.assertEquals(sm, msg);
+            } catch (Exception ignored){}
+        });
+
+        t.start();
+        elevator.sendMessage(Signal.DONE, sm);
+        try {
+            t.join();
+        } catch (InterruptedException ignore){}
     }
 
     @Test
     void receiveMessage() {
-        elevator.sendMessage(request);
-        elevator.receiveMessage();
-        Assertions.assertTrue(sharedBuffer.isBufferEmpty());
+        InetAddress addr = null;
+        try {
+            addr = InetAddress.getLocalHost();
+        }catch (UnknownHostException ignored){}
+
+        assert addr != null;
+        FloorInfoReader.Data data = new FloorInfoReader.Data("4:12", "2", "up", "9");
+        SerializableMessage sm =  new SerializableMessage(addr.getHostAddress(), 8082, Signal.WORK_REQ, MessageTypes.FLOOR, 1, UUID.randomUUID().toString(), UUID.randomUUID().toString(), data);
+
+        Thread t = new Thread(() -> elevator.receiveMessage());
+
+        t.start();
+        try {
+            DatagramSocket sendSocket = new DatagramSocket();
+            SendMessage(sendSocket, sm, InetAddress.getLocalHost(), 8081);
+            t.join();
+        } catch (InterruptedException | IOException ignored){}
     }
-
-
-
-
 }
