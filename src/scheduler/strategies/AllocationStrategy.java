eaf52304d2985934e6ca7ed35e20d0e5f36eb44d
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2024-03-28 18:30:48 -0400

    Sched RMI refac

---
+++ src/scheduler/strategies/AllocationStrategy.java
@@ -0,0 +1,14 @@
+package scheduler.strategies;
+
+import util.SubSystemSharedState;
+import util.WorkAssignment;
+
+import java.util.ArrayList;
+
+public abstract class AllocationStrategy {
+    protected SubSystemSharedState sharedState;
+    AllocationStrategy(SubSystemSharedState sharedState) {
+        this.sharedState = sharedState;
+    }
+    public abstract void allocate(ArrayList<WorkAssignment> workAssignments);
+}
