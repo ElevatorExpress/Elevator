b95d7aae685ecaa6b689ea7dc1396690e64210cd
Yasir Sheikh <ysheikh1234@gmail.com>
2024-04-07 17:11:52 -0400

    adding two more test cases

--- test/scheduler/strategies/LoadBalancedStrategyTest.java
+++ test/scheduler/strategies/LoadBalancedStrategyTest.java
@@ -260,12 +260,20 @@ public class LoadBalancedStrategyTest {
         HashMap<Integer, ConcurrentLinkedDeque<WorkAssignment>> workAssignments = new HashMap<>();
         ConcurrentLinkedDeque<WorkAssignment> workAssignmentsQueue = new ConcurrentLinkedDeque<>();
 
-        // requests for testing - expected order should be 2,3,3,4,5,6,6,7,7,8
-        WorkAssignment wa1 = new WorkAssignment(2, 3, "123", Direction.UP, "request1", "localhost", 8080, Signal.WORK_REQ, 0);
+        // requests for testing - expected order should be 2,3,4,4,4,5,6,6,7,8,8,8,10,11,12,13,15,15,15,15,17,18
+        WorkAssignment wa1 = new WorkAssignment(2, 4, "115", Direction.UP, "request1", "localhost", 8080, Signal.WORK_REQ, 0);
         WorkAssignment wa2 = new WorkAssignment(3,4,"234", Direction.UP, "request2", "localhost", 8080, Signal.WORK_REQ, 0);
-        WorkAssignment wa3 = new WorkAssignment(5, 6, "345", Direction.UP, "request3", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa3 = new WorkAssignment(4, 5, "345", Direction.UP, "request3", "localhost", 8080, Signal.WORK_REQ, 0);
+
         WorkAssignment wa4 = new WorkAssignment(6, 7, "375", Direction.UP, "request4", "localhost", 8080, Signal.WORK_REQ, 0);
-        WorkAssignment wa5 = new WorkAssignment(7, 8, "435", Direction.UP, "request5", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa5 = new WorkAssignment(6, 8, "435", Direction.UP, "request5", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa6 = new WorkAssignment(8, 10, "535", Direction.UP, "request6", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa7 = new WorkAssignment(8, 11, "635", Direction.UP, "request7", "localhost", 8080, Signal.WORK_REQ, 0);
+
+        WorkAssignment wa8 = new WorkAssignment(12, 15, "775", Direction.UP, "request8", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa9 = new WorkAssignment(13, 15, "835", Direction.UP, "request9", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa10 = new WorkAssignment(15, 17, "935", Direction.UP, "request10", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa11 = new WorkAssignment(15, 18, "1035", Direction.UP, "request11", "localhost", 8080, Signal.WORK_REQ, 0);
 
         // Adding requests to work assignment queue
         workAssignmentsQueue.add(wa1);
@@ -273,6 +281,54 @@ public class LoadBalancedStrategyTest {
         workAssignmentsQueue.add(wa3);
         workAssignmentsQueue.add(wa4);
         workAssignmentsQueue.add(wa5);
+        workAssignmentsQueue.add(wa6);
+        workAssignmentsQueue.add(wa7);
+        workAssignmentsQueue.add(wa8);
+        workAssignmentsQueue.add(wa9);
+        workAssignmentsQueue.add(wa10);
+        workAssignmentsQueue.add(wa11);
+
+        //Adding queue to hashmap
+        workAssignments.put(elevatorId, workAssignmentsQueue);
+
+        //Setting shared state work assignments
+        sharedState.setWorkAssignments(workAssignments);
+        //Yasir: Fix this portion - no idea how allocateImproved works
+    }
+
+    @Test
+    void testOverlappingRequestsInDownDirection3(){
+        //Setup elevatorState's floorStopQueue
+        HashMap<Integer, ConcurrentLinkedDeque<WorkAssignment>> workAssignments = new HashMap<>();
+        ConcurrentLinkedDeque<WorkAssignment> workAssignmentsQueue = new ConcurrentLinkedDeque<>();
+
+        // requests for testing - expected order should be 18,17,15,15,15,13,12,12,11,10,10,10,9,8,7,6,5,5,5,5,3,2
+        WorkAssignment wa1 = new WorkAssignment(18, 15, "123", Direction.DOWN, "request1", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa2 = new WorkAssignment(17,15,"234", Direction.DOWN, "request2", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa3 = new WorkAssignment(15, 13, "345", Direction.DOWN, "request3", "localhost", 8080, Signal.WORK_REQ, 0);
+
+        WorkAssignment wa4 = new WorkAssignment(12, 11, "375", Direction.DOWN, "request4", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa5 = new WorkAssignment(12, 10, "435", Direction.DOWN, "request5", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa6 = new WorkAssignment(10, 9, "675", Direction.DOWN, "request6", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa7 = new WorkAssignment(10, 8, "735", Direction.DOWN, "request7", "localhost", 8080, Signal.WORK_REQ, 0);
+
+        WorkAssignment wa8 = new WorkAssignment(7, 5, "775", Direction.DOWN, "request8", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa9 = new WorkAssignment(6, 5, "835", Direction.DOWN, "request9", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa10 = new WorkAssignment(5, 3, "935", Direction.DOWN, "request10", "localhost", 8080, Signal.WORK_REQ, 0);
+        WorkAssignment wa11 = new WorkAssignment(5, 2, "1035", Direction.DOWN, "request11", "localhost", 8080, Signal.WORK_REQ, 0);
+
+        // Adding requests to work assignment queue
+        workAssignmentsQueue.add(wa1);
+        workAssignmentsQueue.add(wa2);
+        workAssignmentsQueue.add(wa3);
+        workAssignmentsQueue.add(wa4);
+        workAssignmentsQueue.add(wa5);
+        workAssignmentsQueue.add(wa6);
+        workAssignmentsQueue.add(wa7);
+        workAssignmentsQueue.add(wa8);
+        workAssignmentsQueue.add(wa9);
+        workAssignmentsQueue.add(wa10);
+        workAssignmentsQueue.add(wa11);
 
         //Adding queue to hashmap
         workAssignments.put(elevatorId, workAssignmentsQueue);
